26a8856c674786f90888d585a6b96366fab9aa02
cicero <cicerog.silvestre@gmail.com>
2024-09-27 18:34:08 -0300

    Finalizado valor da variavel soma

---
+++ terceira-resolucao-prossesamento-valor-soma/index.js
@@ -0,0 +1,18 @@
+/* A função calcularSoma calcula a soma dos números de 2 a 12. Ela inicializa INDICE como 12, SOMA como 0 e K como 1. Em um laço while, K é incrementado e seu valor é adicionado a SOMA enquanto for menor que INDICE. Ao final, a função retorna 77 */
+
+const calcularSoma = () => {
+    let INDICE = 12;
+    let SOMA = 0;
+    let K = 1;
+
+    while (K < INDICE) {
+        K = K + 1;
+        SOMA = SOMA + K;
+    }
+
+    return SOMA;
+}
+
+const resultado = calcularSoma();
+console.log(`O valor final da variável SOMA é: ${resultado}`);
+
